34ba5b33b4cf947741ff660eea05765c2e9f8443
azrasahinovic <a.sahinovic@tntsoft.ba>
2022-03-10 12:27:30 +0100

    export pdf of reports

--- src/app/components/logs/logs.component.ts
+++ src/app/components/logs/logs.component.ts
@@ -130,12 +130,10 @@ export class LogsComponent implements OnInit {
       let PDF = new jsPDF('p', 'mm', 'a4');
       let position = 0;
       PDF.addImage(FILEURI, 'PNG', 0, position, fileWidth, fileHeight);
-      PDF.save('angular-demo.pdf');
+      PDF.save('employees-report.pdf');
     });
     console.log('downloading...')
   }
-
-  
 
   
 }
